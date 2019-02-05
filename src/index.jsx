0dd5460b326ecccf59721ad1ff52fe0633488a33
Veselina Kolova <vmkolova@gmail.com>
2019-02-05 11:30:59 +0200

    Initial config

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './css/index-styles.scss';
+
+ReactDOM.render(
+    <p>
+        <span className='icon icon-text-align-left'/>
+        <h1>Hello world</h1>
+    </p>,
+    document.getElementById('root')
+);
